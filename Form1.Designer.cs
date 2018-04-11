53988414dafc964ba6d6d7feaa2adef227a31dc8
DevRanasinghe <38273018+DevRanasinghe@users.noreply.github.com>
2018-04-11 11:23:49 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Servo_control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtValue3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(22, 107);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 20);
+            this.txtValue1.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(156, 177);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send values";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM14";
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(142, 107);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 20);
+            this.txtValue2.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtValue3
+            // 
+            this.txtValue3.Location = new System.Drawing.Point(264, 107);
+            this.txtValue3.Name = "txtValue3";
+            this.txtValue3.Size = new System.Drawing.Size(100, 20);
+            this.txtValue3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 343);
+            this.Controls.Add(this.txtValue3);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtValue1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.Button btnSend;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtValue3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Servo_control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            
+            
+            if(serialPort1.IsOpen == false)
+            serialPort1.Open();
+              
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+            timer1.Interval = 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                char n = Convert.ToChar(Convert.ToInt32(txtValue1.Text));
+                char m = Convert.ToChar(Convert.ToInt32(txtValue2.Text));
+                char p = Convert.ToChar(Convert.ToInt32(txtValue3.Text));
+                serialPort1.Write(n.ToString());
+                serialPort1.Write(m.ToString());
+                serialPort1.Write(p.ToString());
+                serialPort1.Close();
+            }
+            
+        }
+    }
+}
